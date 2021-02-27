a4f52c6a94ee644487bd766ee6b68f127d36a128
Jonas Neubürger <jonas793@gmail.com>
2021-02-27 13:48:32 +0100

    delete nrw before 2019 because no data

--- program.py
+++ program.py
@@ -11,45 +11,96 @@ import time
 #start timer
 start_time = time.perf_counter()
 #read the data
-df_2019 = pd.read_csv('data/Unfallorte2019_EPSG25832_CSV/csv/Unfallorte2019_LinRef.txt', sep=';')
-toc = time.perf_counter()
-print(f'reading the csv took {toc-start_time:0.4f} seconds.')
+def read_input(location):
+    tic = time.perf_counter()
+    df = pd.read_csv(location, sep=';')
+    toc = time.perf_counter()
+    filename = location[location.rfind('/')+1:location.rfind('.')] 
+    print(f'reading the {filename} csv took {toc-tic:0.4f} seconds.')
+    df.name = filename
+    return df
+
+df_2019 = read_input('data/Unfallorte2019_EPSG25832_CSV/csv/Unfallorte2019_LinRef.txt')
+df_2018 = read_input('data/Unfallorte2018_EPSG25832_CSV/csv/Unfallorte2018_LinRef.txt')
+df_2017 = read_input('data/Unfallorte2017_EPSG25832_CSV/csv/Unfallorte2017_LinRef.txt')
 
 #reformat koordinates, replace , with .
-tic = time.perf_counter()
-for col in ['XGCSWGS84', 'YGCSWGS84']:
-    df_2019[col] = pd.to_numeric(df_2019[col].apply(lambda x: re.sub(',' , '.', str(x))))
-toc = time.perf_counter()
-print(f'replacing all commas with dots took {toc-tic:0.4f} seconds.')
+def replace_commas(df):
+    tic = time.perf_counter()
+    for col in ['XGCSWGS84', 'YGCSWGS84']:
+        df[col] = pd.to_numeric(df[col].apply(lambda x: re.sub(',' , '.', str(x))))
+    toc = time.perf_counter()
+    print(f'replacing all commas in {df.name} with dots took {toc-tic:0.4f} seconds.')
 
-#filter for nrw
-tic = time.perf_counter()
-df_2019_nrw = df_2019.loc[df_2019['ULAND'] == 5 ]
-toc = time.perf_counter()
-print(f'filtering for nrw took {toc-tic:0.4f} seconds.')
+replace_commas(df_2019)
+replace_commas(df_2018)
+replace_commas(df_2017)
+
+#filter for state
+class State():
+    SchleswigHolstein = 1
+    Hamburg = 2
+    Niedersachsen = 3
+    Bremen = 4
+    NordrheinWestfalen = 5
+    Hessen = 6
+    RheinlandPfalz = 7
+    BadenWürttemberg = 8
+    Bayern = 9
+    Saarland = 10
+    Berlin = 11
+    Brandenburg = 12
+    MecklenburgVorpommern = 13
+    Sachsen = 14
+    SachsenAnhalt = 15
+    Thüringen = 16
+
+def filter_state(state_name_nochars, df):
+    tic = time.perf_counter()
+    df_filtered = df.loc[df['ULAND'] == int(state_name_nochars)]
+    toc = time.perf_counter()
+    print(f'filtering for {State(state_name_nochars)} took {toc-tic:0.4f} seconds.')
+    df_filtered.name = df.name + '_state'
+    return df_filtered
+
+# get nrw values
+df_2019_nrw = filter_state(State.NordrheinWestfalen, df_2019)
+
 
 #filter for dortmund, AGS = 05 9 13 000 BL Regbez Kreis Gemeinde
-tic = time.perf_counter()
-df_2019_dortmund = df_2019_nrw.loc[(df_2019_nrw['UREGBEZ'] == 9) & (df_2019_nrw['UKREIS'] == 13)]
-toc = time.perf_counter()
-print(f'filtering for dortmund took {toc-tic:0.4f} seconds.')
+def filter_town_ags(ags, df):
+    tic = time.perf_counter()
+    df_filtered = df.loc[(df['UREGBEZ'] == int(ags[1])) & (df['UKREIS'] == int(ags[2:4]))]
+    toc = time.perf_counter()
+    print(f'filtering for {ags} took {toc-tic:0.4f} seconds.')
+    df_filtered.name = df.name + '_town'
+    return df_filtered
+
+df_2019_nrw_dortmund = filter_town_ags('5913000', df_2019_nrw)
 
 # only bikes
-tic = time.perf_counter()
-df_2019_dortmund_bike = df_2019_dortmund.loc[df_2019_dortmund['IstRad'] == 1]
-toc = time.perf_counter()
-print(f'filtering for bikes took {toc-tic:0.4f} seconds.')
+def filter_bike(df):
+    tic = time.perf_counter()
+    df_filtered = df.loc[df['IstRad'] == 1]
+    toc = time.perf_counter()
+    print(f'filtering for bikes took {toc-tic:0.4f} seconds.')
+    df_filtered.name = df.name + '_bike'
+    return df_filtered
+
+df_2019_nrw_dortmund_bike = filter_bike(df_2019_nrw_dortmund)
 
 # find the map center
 def findcenter(df, lat_name, lon_name):
+    tic = time.perf_counter()
     lat_center = (df[lat_name].min()+df[lat_name].max())/2
     lon_center = (df[lon_name].min()+df[lon_name].max())/2
+    toc = time.perf_counter()
+    print(f'finding the map center {toc-tic:0.4f} seconds.')
     return (lat_center,lon_center)
 
-tic = time.perf_counter()
-center = findcenter(df_2019_dortmund_bike, 'YGCSWGS84', 'XGCSWGS84')
-toc = time.perf_counter()
-print(f'finding the center {toc-tic:0.4f} seconds.')
+#one is enough, no big spread
+center = findcenter(df_2019_nrw_dortmund_bike, 'YGCSWGS84', 'XGCSWGS84')
+print(center)
 
 #load map
 tic = time.perf_counter()
@@ -59,9 +110,17 @@ map = folium.Map(location = center,
 toc = time.perf_counter()
 print(f'creating the map took {toc-tic:0.4f} seconds.')
 
+# add all dfs to a list of dfs, tuple for color
+class YearColor:
+    color2019 = '009988'
+    color2018 = 'EE7733'
+    color2017 = 'EE3377'
+    
+df_list = [(df_2019_nrw_dortmund_bike, YearColor.color2019)]
+
 # add markers
+marker_cluster = MarkerCluster().add_to(map)
 def add_markers(df, df_label, lat_name, lon_name, map, color, iconcolor):
-    marker_cluster = MarkerCluster().add_to(map)
     for idx, row in df.iterrows():
         popup = folium.Popup(row[df_label], max_width=450,min_width=100)
         folium.Marker(
@@ -70,10 +129,12 @@ def add_markers(df, df_label, lat_name, lon_name, map, color, iconcolor):
                         clustered_marker=True,
                         icon=folium.Icon(color = color, icon_color=iconcolor, icon= 'bicycle', prefix='fa')).add_to(marker_cluster)
 
-tic = time.perf_counter()
-add_markers(df_2019_dortmund_bike, 'UART', 'YGCSWGS84', 'XGCSWGS84', map, 'white', '009988')
-toc = time.perf_counter()
-print(f'adding clustered markers to the map took {toc-tic:0.4f} seconds.')
+
+for i in range(len(df_list)):
+    tic = time.perf_counter()
+    add_markers(df_list[i][0], 'UART', 'YGCSWGS84', 'XGCSWGS84', map, 'white', df_list[i][1])
+    toc = time.perf_counter()
+    print(f'adding clustered markers of {df_list[i][0].name} to the map took {toc-tic:0.4f} seconds.')
 
 #save map as html
 tic = time.perf_counter()
@@ -83,12 +144,14 @@ print(f'saving the map took {toc-tic:0.4f} seconds.')
 
 tic = time.perf_counter()
 print(f'everything took {toc-start_time:0.4f} seconds.')
+
+
 #debugging prints:
 debug = False
 if debug == True:
     print((df_2019_nrw['UREGBEZ']))
     print((df_2019_nrw['UKREIS']))
     print(df_2019)
-    print(df_2019_dortmund)
-    print(df_2019_dortmund_bike)
+    print(df_2019_nrw_dortmund)
+    print(df_2019_nrw_dortmund_bike)
     print(center)
